5037186c2d4a99e341fb0906a459d67e1075e94c
Benedikt Waldvogel <benedikt.waldvogel@cronn.de>
2024-09-24 14:53:44 +0200

    Apply project code formatting convention in Action

--- src/main/java/de/cronn/commons/lang/Action.java
+++ src/main/java/de/cronn/commons/lang/Action.java
@@ -3,27 +3,29 @@ package de.cronn.commons.lang;
 import java.util.concurrent.Callable;
 import java.util.function.Supplier;
 
-/** Similar to {@link Runnable} but it allows to throw checked exceptions. */
+/**
+ * Similar to {@link Runnable} but it allows to throw checked exceptions.
+ */
 @FunctionalInterface
 public interface Action {
 
-  void execute() throws Exception;
+	void execute() throws Exception;
 
-  default Supplier<Void> toSupplier() {
-    return () -> {
-      try {
-        execute();
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-      return null;
-    };
-  }
+	default Supplier<Void> toSupplier() {
+		return () -> {
+			try {
+				execute();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+			return null;
+		};
+	}
 
-  default Callable<Void> toCallable() {
-    return () -> {
-      execute();
-      return null;
-    };
-  }
+	default Callable<Void> toCallable() {
+		return () -> {
+			execute();
+			return null;
+		};
+	}
 }
